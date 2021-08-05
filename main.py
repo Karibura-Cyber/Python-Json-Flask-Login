decb438be9ba97e666bb6e1bd20e1dd48642aae1
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2021-08-05 16:52:46 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import json
 import base64
 
 app = Flask(__name__)
-
+if not os.path.exists('data'):
+    os.makedirs('data')
 @app.route('/')
-def my_form():
+def index():
+    return render_template('index.html')
+
+@app.route('/signin')
+def signin():
     return render_template('signin.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route('/signin', methods=['POST'])
+def signin_post():
     name = request.form['name']
     passwd = request.form['passwd']
     
@@ -18,15 +23,42 @@ def my_form_post():
     base64_bytes = base64.b64encode(passwd_bytes)
     base64_passwd = base64_bytes.decode('ascii')
 
-    #os check directory
-    if not os.path.exists('data'):
-        os.makedirs('data')
-        with open('data/{}_data.json'.format(name), 'w') as outfile:
-            json.dump({'name': name, 'passwd': base64_passwd}, outfile)
+    #os check file
+    if os.path.exists('data/{}_data.json'.format(name)):
+        with open('data/{}_data.json'.format(name), 'r') as f:
+            i = json.load(f)
+            
+    elif not os.path.exists('data/{}_data.json'.format(name)):
+        return 'user not found'
+
+    if i["username"] == name and i["passwd"] == base64_passwd:
+        return 'Welcome {}'.format(name)
     else:
-        with open('data/{}_data.json'.format(name), 'w') as outfile:
-            json.dump({'name': name, 'passwd': base64_passwd}, outfile)
+        return 'Invalid username or password'
+   
+    
 
 
-    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signup', methods=['POST'])
+def signup_post():
+    name = request.form['name']
+    passwd = request.form['passwd']
+    
+    passwd_bytes = passwd.encode('ascii')
+    base64_bytes = base64.b64encode(passwd_bytes)
+    base64_passwd = base64_bytes.decode('ascii')
+
+
+ 
+    with open('data/{}_data.json'.format(name), 'w') as outfile:
+       json.dump({'username': name, 'passwd': base64_passwd}, outfile)
+       
+    return ''
+
+    
     
