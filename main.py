a26f3dae8dba34fa20577c53b7fdb51aa4aaa40a
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2021-08-08 20:08:18 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 import os
 import os
@@ -11,8 +11,29 @@ if not os.path.exists('data'):
     os.makedirs('data')
 
 
-app = Flask(__name__)
-app.config["IMAGE_UPLOADS"] = "static/uploads/"
+app=Flask(__name__)
+
+app.secret_key = "secret key"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+path = os.getcwd()
+# file Upload
+UPLOAD_FOLDER = os.path.join(path, 'static/uploads')
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
 
 
 @app.route('/')
@@ -69,9 +90,8 @@ def signup_post():
         return 'user already exists <a href="/signup">Sign Up</a>'
     elif not os.path.exists('data/{}_data.json'.format(name)):
         with open('data/{}_data.json'.format(name), 'w') as outfile:
-            json.dump({'username': name, 'passwd': base64_passwd, 'img': '', 'score': 0}, outfile)
-        with open('data{}_data.json'.format(name), 'w') as outfile:
-            json.dump({'username': name, 'img': '', 'score': 0}, outfile)
+            json.dump({'username': name, 'passwd': base64_passwd, 'img': 'uploads/{}.jpg'.format(name), 'score': 0}, outfile)
+
        
        
     return  "<script>window.location.href = '/signin';</script>"
@@ -89,7 +109,35 @@ def userinfo(name):
         score = 'Error'
     else:
         score = i["score"]
-    return render_template('userinfo.html', name=i["username"], score=score, img=i["img"])
+        img = i["img"]
+    return render_template('userinfo.html', name=name, score=score, img=img)
+
+@app.route('/userinfo/upload/<name>')
+def upload_form(name):
+    return render_template('upload.html', name=name)
+
+
+@app.route('/userinfo/upload/<name>', methods=['POST'])
+def upload_file(name):
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            #os rename file
+            os.rename(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(app.config['UPLOAD_FOLDER'], '{}.jpg'.format(name)))
+            return redirect('/userinfo/{}'.format(name))
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
 
 @app.route('/api/<name>')
 def api(name):
@@ -97,4 +145,10 @@ def api(name):
         with open('data/{}_data.json'.format(name), 'r') as f:
             i = json.load(f)
     
-    return json.dumps({'username': i["username"], 'img': i["img"], 'score': i["score"]})
+    return json.dumps({'username': i["username"], 'score': i["score"]})
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
