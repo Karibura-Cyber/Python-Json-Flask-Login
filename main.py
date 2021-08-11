345c41138e69a852f61a80ae8604d2b5838cd92d
Meck <76256502+Karibura-Cyber@users.noreply.github.com>
2021-08-11 19:22:04 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import json
 import base64
 import time
-
+import smtplib
 
 if not os.path.exists('data'):
     os.makedirs('data')
@@ -79,7 +79,7 @@ def signup():
 def signup_post():
     name = request.form['name']
     passwd = request.form['passwd']
-    
+    email = request.form['email']
     passwd_bytes = passwd.encode('ascii')
     base64_bytes = base64.b64encode(passwd_bytes)
     base64_passwd = base64_bytes.decode('ascii')
@@ -90,7 +90,7 @@ def signup_post():
         return 'user already exists <a href="/signup">Sign Up</a>'
     elif not os.path.exists('data/{}_data.json'.format(name)):
         with open('data/{}_data.json'.format(name), 'w') as outfile:
-            json.dump({'username': name, 'passwd': base64_passwd, 'img': 'uploads/{}.jpg'.format(name), 'score': 0}, outfile)
+            json.dump({'username': name, 'passwd': base64_passwd, 'email': email, 'img': 'uploads/{}.jpg'.format(name), 'score': 0}, outfile)
 
        
        
@@ -145,10 +145,57 @@ def api(name):
         with open('data/{}_data.json'.format(name), 'r') as f:
             i = json.load(f)
     
-    return json.dumps({'username': i["username"], 'score': i["score"]})
+    return json.dumps({'username': i["username"], 'email': i["email"], 'score': i["score"]})
 
+@app.route('/forget')
+def forget():
+    return render_template('forget.html')
 
+@app.route('/forget', methods=['POST'])
+def forget_post():
+    name = request.form['name']
+    if os.path.exists('data/{}_data.json'.format(name)):
+        with open('data/{}_data.json'.format(name), 'r') as f:
+            i = json.load(f)
+        return render_template('confirm_forget.html', name=name, email=i["email"])
+    elif not os.path.exists('data/{}_data.json'.format(name)):
+        return 'user not found <a href="/signup">Sign Up</a>'
 
+@app.route('/forget/send/<name>')
+def forget_send(name):
+    if os.path.exists('data/{}_data.json'.format(name)):
+        with open('data/{}_data.json'.format(name), 'r') as f:
+            i = json.load(f)
 
+        gmail_user = 'kariburahack@gmail.com' #replace your Gmail Here [Gmail Only for now]
+        gmail_password = 'meck1201'
+        reciever = i["email"]
+        passwd = base64.b64decode(i["passwd"])
+        sent_from = gmail_user
+        to = ['{}'.format(reciever)]
+        subject = 'Python Flask JSON Login system forget your password'
+        body = "Hey, what's up?\n\n this email send from Python Flask JSON Login system \n\n this is your password: {} \n\n This method is beta".format(passwd)
+
+        email_text = """\
+        From: %s
+        To: %s
+        Subject: %s
+
+        %s
+        """ % (sent_from, ", ".join(to), subject, body)
+
+        try:
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(gmail_user, gmail_password)
+            server.sendmail(sent_from, to, email_text)
+            server.close()
+        except:
+            print('Something went wrong...')
+        return redirect('/')
+
+    #check file 
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
